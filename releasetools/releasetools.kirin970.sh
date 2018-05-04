f07c86b6005a7c7f9247ceb61892b730ab7ac721
Paul Keith <javelinanddart@gmail.com>
2018-05-04 04:48:26 +0200

    berkeley: Remount system as RW in our releasetools extension
    * This is usually not necessary but sometimes TWRP
      wants to keep /system read only and because of
      that our releasetools can't modify anything.

--- releasetools/releasetools.kirin970.sh
+++ releasetools/releasetools.kirin970.sh
@@ -3,6 +3,9 @@
 # Remount system as R/W
 mount -o rw,remount /system
 
+# Remount system as R/W
+mount -o rw,remount /system
+
 # Remove duplicated genfscon rules
 sed -i "/genfscon exfat/d" /system/etc/selinux/plat_sepolicy.cil
 sed -i "/genfscon fuseblk/d" /system/etc/selinux/plat_sepolicy.cil

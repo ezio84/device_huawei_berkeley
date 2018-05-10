4338deb3b1975ccf34aa8574406c02c4784fff79
camcory <camcory1@gmail.com>
2018-05-10 18:22:38 +0200

    Fix build, enable navbar by default

--- aosp_berkeley.mk
+++ aosp_berkeley.mk
@@ -14,11 +14,16 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/huawei/berkeley/full_berkeley.mk)
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/huawei/berkeley/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/treble_common.mk)
+
+# Inherit from berkeley device
+$(call inherit-product, device/huawei/berkeley/device.mk)
 
 PRODUCT_NAME := aosp_berkeley
 PRODUCT_DEVICE := berkeley
